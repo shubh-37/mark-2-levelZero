f9ed70506b6c33aff249e3b045060f7ebd1f89df
shubh arya <74173755+shubh-37@users.noreply.github.com>
2022-09-22 07:02:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+var question = "What is your name? ";
+
+var userAnswer = readlineSync.question(question);
+
+console.log("Welcome "+ userAnswer + " to DO YOU KNOW SHUBH? ");
+
+function myquiz(question, answer){
+  var userAns = readlineSync.question(question);
+  if(userAns == answer){
+    console.log("YAY its correct!!")
+    score = score+1;
+  }else{
+    console.log("OOPS incorrect :((");
+  }
+}
+
+var quizArr = [
+  {
+    question: "What is my favorite food? ",
+    answer: "pizza",
+  },
+  {
+    question: "What is my favorite tv show? ",
+    answer: "game of thrones"
+  },
+  {
+    question: "Who is my favorite player in cricket? ",
+    answer: "sehwag"
+  }
+]
+
+for (let i = 0; i < quizArr.length; i++){
+  var currentQues = quizArr[i];
+  console.log(myquiz(currentQues.question, currentQues.answer))
+}
+
+console.log("Your Score is " + score);
